728474ea4f9f0a1022a9ce7925a35def73b56e8c
Nick Triantafylldis <ntrianta90@gmail.com>
2015-06-03 18:45:37 +0200

    parse flows

--- main.go
+++ main.go
@@ -7,13 +7,26 @@ import (
 )
 
 func main() {
-	if handle, err := pcap.OpenOffline("/path/to/file"); err != nil {
+	if handle, err := pcap.OpenOffline("/path/to/file.pcap"); err != nil {
 		panic(err)
 	} else {
 		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 		for packet := range packetSource.Packets() {
-
-			fmt.Println(packet)
+			if packet == nil {
+				fmt.Println("empty packet")
+			} else if packet.TransportLayer() == nil {
+				fmt.Println("no transport")
+			} else {
+				link := packet.LinkLayer()
+				linkFlow := link.LinkFlow()
+				net := packet.NetworkLayer()
+				netFlow := net.NetworkFlow()
+				trans := packet.TransportLayer()
+				transFlow := trans.TransportFlow()
+				//	app := packet.ApplicationLayer()
+				fmt.Println(linkFlow, netFlow, transFlow, packet.Metadata().Timestamp)
+				//	fmt.Println(app)
+			}
 		}
 
 	}
