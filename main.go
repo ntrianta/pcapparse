de253331586e5ad04c51565e78998fa7ee9e7af1
Nick Triantafylldis <ntrianta90@gmail.com>
2015-06-03 16:13:39 +0200

    decode pcap

--- main.go
+++ main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
-    "code.google.com/p/gopacket/pcap"
-    "fmt"
+	"code.google.com/p/gopacket"
+	"code.google.com/p/gopacket/pcap"
+	"fmt"
 )
 
-func main(){
-      if handle, err := pcap.OpenOffline("/path/to/my/file"); err != nil {
-      panic(err)
-    } else {
-  packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-  for packet := range packetSource.Packets() {
-    handlePacket(packet)  // Do something with a packet here.
-  }
-}
+func main() {
+	if handle, err := pcap.OpenOffline("/path/to/file"); err != nil {
+		panic(err)
+	} else {
+		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+		for packet := range packetSource.Packets() {
+
+			fmt.Println(packet)
+		}
+
+	}
 
 }
